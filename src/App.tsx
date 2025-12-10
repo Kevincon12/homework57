19fb8da51a4343d512dd1051ff465a62b4253661
Strelnikov Ilia <ily_strelnikov@mail.ru>
2025-12-10 13:42:44 +0600

    Добавили тулбар

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import Toolbar from "./components/UI/Toolbar/Toolbar.tsx";
 
 const App = () => {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-
+        <header>
+            <Toolbar />
+        </header>
     </>
   )
 };
---
+++ src/components/UI/Toolbar/Toolbar.tsx
@@ -0,0 +1,12 @@
+
+const Toolbar = () => {
+    return (
+        <nav className='navbar navbar-dark navbar-fixed-top bg-primary'>
+            <div className='container-fluid'>
+                <span className='navbar-brand'>Homework 57</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Toolbar;

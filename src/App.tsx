59e2bae5a81cd711e4d00a18767cfcdf334d6e64
Strelnikov Ilia <ily_strelnikov@mail.ru>
2025-12-10 19:47:38 +0600

    Подготовили компонент UserItem, отвечающий за показ одного пользователя

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,25 @@ import Users from "./components/Users/Users.tsx";
 import type {User} from './types';
 
 const App = () => {
-  const [user, setUser] = useState<User[]>([]);
+  const [user, setUser] = useState<User[]>([
+      {
+          id: '1',
+          name: 'sdadsda',
+          email: 'iasdadasdad.com',
+          online: true,
+          role: 'Admin'
+      },
+      {
+          id: '2',
+          name: 'sadsadads2',
+          email: 'sdsadasdadwdse.com',
+          online: false,
+          role: 'Editor'
+      }
+  ]);
 
   const addUser = (user: User) => {
       setUser(prevState => [...prevState, user]);
-      console.log(user);
   };
 
   return (
--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import type {User} from '../../types';
 
-const UserItem = () => {
+interface Props {
+    user: User;
+}
+
+const UserItem: React.FC<Props> = ({user}) => {
     return (
-        <div>
-            
+        <div className='card mb-2 p-2'>
+            <h3 className='card-title'>Name: {user.name}</h3>
+            <p className='card-text'>Email: {user.email}</p>
+            <p className='card-text'>Role: {user.role}</p>
+            <p className='card-text'>Status: {user.online ? 'Online' : 'Offline'}</p>
         </div>
     );
 };
